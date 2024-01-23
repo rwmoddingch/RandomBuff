23b028ec60970ff708dd0cb8a51019e852dc027f
Tricrystal <34974954+pkuyo@users.noreply.github.com>
2024-01-23 19:38:05 +0800

    buff基础框架
    基于存档信息的保存加载，hook加载
    entry加载

--- RandomBuff/BuffPlugin.cs
+++ RandomBuff/BuffPlugin.cs
@@ -1,18 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
+using BepInEx;
+using RandomBuff.Core.Entry;
+using RandomBuff.Core.Hooks;
+using RandomBuff.Core.SaveData;
 using UnityEngine;
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+//添加友元方便调试
+[assembly: InternalsVisibleTo("BuffTest")]
 
 namespace RandomBuff
 {
-    internal class BuffPlugin
+    [BepInPlugin("randombuff", "Random Buff", "1.0.0")]
+    class BuffPlugin : BaseUnityPlugin
     {
+        public const string saveVersion = "a-0.0.1";
 
-        public static void Log(string s)
+        public void OnEnable()
         {
-            Debug.Log($"[RandomBuff]{s}");
+            On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+        }
+
+        private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            try
+            {
+                orig(self);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+            try
+            {
+                if (!isLoaded)
+                {
+               
+
+                    if(File.Exists(AssetManager.ResolveFilePath("randomBuff.log")))
+                        File.Delete(AssetManager.ResolveFilePath("randomBuff.log"));
+                    File.Create(AssetManager.ResolveFilePath("randomBuff.log")).Close();
+
+                    Log($"[Random Buff], version: {saveVersion}");
+
+                    if (File.Exists(AssetManager.ResolveFilePath("buff.dev")))
+                    {
+                        DevEnabled = true;
+                        LogWarning("Debug Enable");
+                    }
+
+                    BuffFile.OnModsInit();
+                    CoreHooks.OnModsInit();
+                    BuffRegister.InitAllBuffPlugin();
+               
+                    isLoaded = true;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.LogException(e);
+            }
+        }
+
+        private static bool isLoaded = false;
+
+
+        internal static bool DevEnabled { get; private set; }
+
+
+        public static void Log(object message)
+        {
+            Debug.Log($"[RandomBuff] {message}");
+            File.AppendAllText(AssetManager.ResolveFilePath("randomBuff.log"), $"[Message]\t{message}\n");
+        }
+
+        public static void LogWarning(object message)
+        {
+            Debug.LogWarning($"[RandomBuff] {message}");
+            File.AppendAllText(AssetManager.ResolveFilePath("randomBuff.log"), $"[Warning]\t{message}\n");
+        }
+
+        public static void LogError(object message)
+        {
+            Debug.LogError($"[RandomBuff] {message}");
+            File.AppendAllText(AssetManager.ResolveFilePath("randomBuff.log"), $"[Error]\t{message}\n");
         }
     }
 }
---
+++ RandomBuff/Core/Entry/BuffRegister.cs
@@ -0,0 +1,100 @@
+﻿#define BUFFDEBUG 
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MonoMod.RuntimeDetour;
+using RandomBuff.Core.Buff;
+
+
+namespace RandomBuff.Core.Entry
+{
+
+    /// <summary>
+    /// 保存Buff的注册信息，负责保存BuffData/Buff的Type信息
+    /// 不保存实际的data类型
+    /// 不随存档变化刷新
+    ///
+    /// 外部接口
+    /// </summary>
+    public static partial class BuffRegister
+    {
+        public static void RegisterBuff<BuffType, DataType, HookType>(BuffID id) where BuffType : IBuff, new()
+            where DataType : BuffData, new()
+        {
+            BuffTypes.Add(id, typeof(BuffType));
+            DataTypes.Add(id, typeof(DataType));
+            BuffHookWarpper.RegisterHook(id, typeof(HookType));
+        }
+
+        public static void RegisterBuff<BuffType, DataType>(BuffID id) where BuffType : IBuff, new()
+            where DataType : BuffData, new()
+        {
+            BuffTypes.Add(id, typeof(BuffType));
+            DataTypes.Add(id, typeof(DataType));
+        }
+    }
+
+    /// <summary>
+    /// 保存Buff的注册信息，负责保存BuffData/Buff的Type信息
+    /// 不保存实际的data类型
+    /// 不随存档变化刷新
+    /// </summary>
+    public static partial class BuffRegister
+    {
+      
+
+        internal static (BuffID id, Type type) GetDataType(string id) => 
+            (new BuffID(id),GetAnyType(new BuffID(id), DataTypes));
+
+        internal static (BuffID id, Type type) GetBuffType(string id) => 
+            (new BuffID(id), GetAnyType(new BuffID(id), BuffTypes));
+
+        internal static Type GetDataType(BuffID id) => GetAnyType(id, DataTypes);
+
+        internal static Type GetBuffType(BuffID id) => GetAnyType(id, BuffTypes);
+
+        private static Type GetAnyType(BuffID id, Dictionary<BuffID, Type> dic)
+        {
+            if (dic.ContainsKey(id))
+                return dic[id];
+            return null;
+        }
+
+
+        internal static void InitAllBuffPlugin()
+        {
+            foreach (var mod in ModManager.ActiveMods)
+            {
+                string path = mod.path + Path.DirectorySeparatorChar + "buffplugins";
+                if (!Directory.Exists(path))
+                    continue;
+                BuffPlugin.Log($"Find correct path in {mod.id} to load plugins");
+                DirectoryInfo info = new DirectoryInfo(path);
+                foreach (var file in info.GetFiles("*.dll"))
+                {
+                    var assembly = Assembly.LoadFile(file.FullName);
+                    foreach (var type in assembly.GetTypes())
+                    {
+
+                        if (type.GetInterfaces().Contains(typeof(IBuffEntry)))
+                        {
+                            var obj = type.GetConstructor(Type.EmptyTypes).Invoke(Array.Empty<object>());
+                            type.GetMethod("OnEnable").Invoke(obj, Array.Empty<object>());
+                            BuffPlugin.Log($"Invoke {type.Name}.OnEnable");
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private static Dictionary<BuffID, Type> DataTypes = new();
+        private static Dictionary<BuffID, Type> BuffTypes = new();
+    }
+}
