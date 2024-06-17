a8266f96ad5ce7d2f8bef3008b8d4389e64bf0f7
Harvie Sorroway <1934182494@qq.com>
2024-06-17 19:36:25 +0800

    添加了随机房间buff

---
+++ BuildInBuff/Duality/RandomRoomBuff.cs
@@ -0,0 +1,226 @@
+﻿using RandomBuff.Core.Buff;
+using RandomBuff.Core.Entry;
+using RandomBuffUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Random = UnityEngine.Random;
+
+namespace BuiltinBuffs.Duality
+{
+    internal class RandomRoomBuff : Buff<RandomRoomBuff, RandomRoomBuffData>
+    {
+        public override BuffID ID => RandomRoomBuffEntry.randomRoomBuffID;
+
+
+        bool randomized;
+        List<AbstractRoomRepresent> activeRoomRepresents = new List<AbstractRoomRepresent>();
+
+        public override void Update(RainWorldGame game)
+        {
+            if (!randomized)
+            {
+                RandomizeRoomConnections(game.world);
+                randomized = true;
+            }
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+            foreach(var represent in activeRoomRepresents)
+            {
+                for(int i = 0;i < represent.room.connections.Length;i++)
+                {
+                    represent.room.connections[i] = represent.origConnections[i].index;
+                }
+            }
+        }
+
+        public void ChangeRegion()
+        {
+            activeRoomRepresents.Clear();
+            randomized = false;
+        }
+
+        void RandomizeRoomConnections(World world)
+        {
+            //List<AbstractRoomRepresent> roomRepresents = new List<AbstractRoomRepresent>();
+            List<AbstractRoomRepresent> roomForConnect = new List<AbstractRoomRepresent>();
+
+            foreach (var room in world.abstractRooms)
+            {
+                if (room.connections.Length == 0)
+                    continue;
+
+                var represet = new AbstractRoomRepresent(room);
+                activeRoomRepresents.Add(represet);
+                roomForConnect.Add(represet);
+
+                for(int i = 0;i < room.connections.Length; i++)
+                {
+                    represet.origConnections[i] = world.GetAbstractRoom(room.connections[i]);
+                }
+            }
+
+            //排除业力门相关、单口房间、避难所链接
+            for (int m = activeRoomRepresents.Count - 1; m >= 0; m--)
+            {
+                var matchRepreset = activeRoomRepresents[m];
+                if (matchRepreset.room.gate || matchRepreset.room.shelter)
+                {
+                    for (int i = 0; i < matchRepreset.room.connections.Length; i++)
+                    {
+                        var room = world.GetAbstractRoom(matchRepreset.room.connections[i]);
+
+                        if (room == null)//非该区域房间
+                            continue;
+                        else
+                        {
+                            var newRep = AbstractRoomRepresent.GetRepresentFromList(activeRoomRepresents, room);
+                            for (int j = 0; j < room.connections.Length; j++)
+                            {
+                                if (world.GetAbstractRoom(room.connections[j]) == matchRepreset.room)
+                                    newRep.connectionRepresents[j] = matchRepreset.room;
+                            }
+                            if (newRep.CurrentEmptyConnectionIndex == -1)
+                                roomForConnect.Remove(newRep);
+                        }
+                    }
+                    activeRoomRepresents.Remove(matchRepreset);
+                    roomForConnect.Remove(matchRepreset);
+                }
+                if (matchRepreset.room.connections.Length == 1)//单链接房间
+                {
+                    var room = world.GetAbstractRoom(matchRepreset.room.connections[0]);
+                    if (room == null)//非该区域房间
+                        continue;
+                    else
+                    {
+                        var newRep = AbstractRoomRepresent.GetRepresentFromList(activeRoomRepresents, room);
+                        for (int j = 0; j < room.connections.Length; j++)
+                        {
+                            if (world.GetAbstractRoom(room.connections[j]) == matchRepreset.room)
+                                newRep.connectionRepresents[j] = matchRepreset.room;
+                        }
+                        if (newRep.CurrentEmptyConnectionIndex == -1)
+                            roomForConnect.Remove(newRep);
+                    }
+                    activeRoomRepresents.Remove(matchRepreset);
+                    roomForConnect.Remove(matchRepreset);
+                }
+            }
+
+            foreach (var roomRepresent in activeRoomRepresents)//随机链接
+            {
+                int index = -1;
+                while ((index = roomRepresent.CurrentEmptyConnectionIndex) != -1 && roomForConnect.Count > 0)
+                {
+                    List<int> randomIndex = new List<int>();
+                    for (int i = 0; i < roomForConnect.Count; i++)
+                        randomIndex.Add(i);
+                    randomIndex.Remove(roomForConnect.IndexOf(roomRepresent));
+                    if (randomIndex.Count == 0)
+                        break;
+
+                    AbstractRoomRepresent randomRoom = roomForConnect[randomIndex[Random.Range(0, randomIndex.Count)]];
+
+                    BuffUtils.Log($"ConnectTest", $"Connect room : {roomRepresent.room.name}_{index}_{roomRepresent.connectionRepresents.Count()} <-> {randomRoom.room.name}_{randomRoom.CurrentEmptyConnectionIndex}_{randomRoom.connectionRepresents.Count()}");
+
+                    roomRepresent.connectionRepresents[index] = randomRoom.room;
+                    randomRoom.connectionRepresents[randomRoom.CurrentEmptyConnectionIndex] = roomRepresent.room;
+
+
+                    if (roomRepresent.CurrentEmptyConnectionIndex == -1)
+                        roomForConnect.Remove(roomRepresent);
+                    if (randomRoom.CurrentEmptyConnectionIndex == -1)
+                        roomForConnect.Remove(randomRoom);
+                }
+            }
+
+            var builder = new StringBuilder("\n");
+            foreach (var roomRepresent in activeRoomRepresents)//debug输出链接
+            {
+                builder.Append($"{roomRepresent.room.name} : ");
+                foreach (var connection in roomRepresent.connectionRepresents)
+                {
+
+
+                    builder.Append(connection == null ? "NONE" : connection.name);
+                    builder.Append(" ");
+                }
+                builder.Append("\n");
+
+                for (int i = 0; i < roomRepresent.connectionRepresents.Length; i++)
+                {
+                    if (roomRepresent.connectionRepresents[i] == null)
+                        continue;
+                    roomRepresent.room.connections[i] = roomRepresent.connectionRepresents[i].index;
+                }
+            }
+            BuffUtils.Log("ConnectionTest", builder.ToString());
+        }
+
+        class AbstractRoomRepresent
+        {
+            public AbstractRoom room;
+            public AbstractRoom[] connectionRepresents;
+            public AbstractRoom[] origConnections;
+
+            public int CurrentEmptyConnectionIndex
+            {
+                get
+                {
+                    for (int i = 0; i < connectionRepresents.Length; i++)
+                        if (connectionRepresents[i] == null)
+                            return i;
+                    return -1;
+                }
+            }
+
+            public AbstractRoomRepresent(AbstractRoom room)
+            {
+                this.room = room;
+                connectionRepresents = new AbstractRoom[room.connections.Length];
+                origConnections = new AbstractRoom[room.connections.Length];
+            }
+
+            public static AbstractRoomRepresent GetRepresentFromList(List<AbstractRoomRepresent> list, AbstractRoom room)
+            {
+                foreach (var rr in list)
+                    if (rr.room == room)
+                        return rr;
+                return null;
+            }
+        }
+    }
+
+    internal class RandomRoomBuffData : CountableBuffData
+    {
+        public override BuffID ID => RandomRoomBuffEntry.randomRoomBuffID;
+
+        public override int MaxCycleCount => 10;
+    }
+
+    internal class RandomRoomBuffEntry : IBuffEntry
+    {
+        public static BuffID randomRoomBuffID = new BuffID("RandomRoom", true);
+        public void OnEnable()
+        {
+            BuffRegister.RegisterBuff<RandomRoomBuff, RandomRoomBuffData, RandomRainIBuffEntry>(randomRoomBuffID);
+        }
+
+        public static void HookOn()
+        {
+            On.WorldLoader.CreatingWorld += WorldLoader_CreatingWorld;
+        }
+
+        private static void WorldLoader_CreatingWorld(On.WorldLoader.orig_CreatingWorld orig, WorldLoader self)
+        {
+            orig.Invoke(self);
+            RandomRoomBuff.Instance.ChangeRegion();
+        }
+    }
+}
