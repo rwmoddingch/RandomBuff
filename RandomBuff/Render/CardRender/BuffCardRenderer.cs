7a9283321d417a08881ee530e58262ef45a2f9a3
Tricrystal <34974954+pkuyo@users.noreply.github.com>
2024-07-28 17:44:52 +0800

    修复PhyX clean the mesh failed报错，修复启用hook顺序问题

--- RandomBuff/Render/CardRender/BuffCardRenderer.cs
+++ RandomBuff/Render/CardRender/BuffCardRenderer.cs
@@ -133,6 +133,9 @@ namespace RandomBuff.Render.CardRender
             _cardQuadFront.GetComponent<MeshRenderer>().material.shader = CardBasicAssets.CardHighlightShader;
             cardHighlightFrontController = _cardQuadFront.AddComponent<CardHighlightController>();
             _meshFilterFront = _cardQuadFront.GetComponent<MeshFilter>();
+            if(_cardQuadFront.TryGetComponent<Collider>(out var collider))
+                Destroy(collider);
+            
             //cardHighlightFrontController.Init(this, _cardTextureFront);
 
             _cardQuadBack = GameObject.CreatePrimitive(PrimitiveType.Quad);
@@ -144,6 +147,10 @@ namespace RandomBuff.Render.CardRender
             _cardQuadBack.GetComponent<MeshRenderer>().material.shader = CardBasicAssets.CardHighlightShader;
             cardHighlightBackController = _cardQuadBack.AddComponent<CardHighlightController>();
             cardHighlightBackController.Init(this, _cardTextureBack, false);
+            if (_cardQuadBack.TryGetComponent<Collider>(out var collider1))
+            {
+                Destroy(collider1);
+            }
 
             Depth = 8.5f;
             Rotation = Vector2.zero;
