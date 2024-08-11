713119d8e156d0b7bdc4e3531cb4bd5d4c9e1d20
ShanKa <Shan_Ka@outlook.com>
2024-08-11 21:15:18 +0200

    我头痛眩晕增加过渡间隔改为30秒到5分钟

--- BuildInBuff/Negative/HeadAche.cs
+++ BuildInBuff/Negative/HeadAche.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 using MonoMod;
+using RandomBuff;
 using RandomBuff.Core.Buff;
 using RandomBuff.Core.Entry;
+using TemplateGains;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace HotDogGains.Negative
 {
-    class HeadAcheBuff : Buff<HeadAcheBuff, HeadAcheBuffData>{public override BuffID  ID => HeadAcheBuffEntry.HeadAcheID;}
-    class HeadAcheBuffData :CountableBuffData
+    class HeadAcheBuff : Buff<HeadAcheBuff, HeadAcheBuffData> { public override BuffID ID => HeadAcheBuffEntry.HeadAcheID; }
+    class HeadAcheBuffData : CountableBuffData
     {
         public override BuffID ID => HeadAcheBuffEntry.HeadAcheID;
 
@@ -19,21 +23,58 @@ namespace HotDogGains.Negative
         public static BuffID HeadAcheID = new BuffID("HeadAcheID", true);
         public void OnEnable()
         {
-            BuffRegister.RegisterBuff<HeadAcheBuff,HeadAcheBuffData,HeadAcheBuffEntry>(HeadAcheID);
+            BuffRegister.RegisterBuff<HeadAcheBuff, HeadAcheBuffData, HeadAcheBuffEntry>(HeadAcheID);
         }
-            public static void HookOn()
+        public static void HookOn()
         {
             On.Player.Update += Player_Update;
         }
-        
+
         private static void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
         {
             orig.Invoke(self, eu);
-            if (Random.Range(0,3000)>2996)
-            {
-                HeadAcheBuff.Instance.TriggerSelf(true);
-                self.Stun(80);
-            }
+
+            self.Ache().AcheUpdate();
+            //if (Random.Range(0,3000)>2996)
+            //{
+            //    HeadAcheBuff.Instance.TriggerSelf(true);
+            //    self.Stun(80);
+            //}
         }
+    }
+    public class AcheData
+    {
+        public int stunCount = Random.Range(40 * 15, 40 * 60);
+        public Player player;
+
+        public AcheData(Player player)
+        {
+            this.player = player;
+        }
+
+        public void RollCount()
+        {
+            stunCount = Random.Range(40 * 30, 40 * 300);
+        }
+        public void AcheStun()
+        {
+            HeadAcheBuff.Instance.TriggerSelf();
+            player.Stun(80);
+            RollCount();
+        }
+        public void AcheUpdate()
+        {
+            stunCount--;
+            if (stunCount < 10 * 40) player.AerobicIncrease(2f);
+            if (stunCount < 3 * 40) player.slowMovementStun=3;
+            if (stunCount <= 0) AcheStun();
+        }
+
+    }
+    public static class GetData
+    {
+        public static readonly ConditionalWeakTable<Player, AcheData> modules = new ConditionalWeakTable<Player, AcheData>();
+
+        public static AcheData Ache(this Player player) => modules.GetValue(player, (Player p) => new AcheData(p));
     }
 }
