655e04c2ee342f95ee01de7d745f5c79d3898d08
ShanKa <Shan_Ka@outlook.com>
2024-08-11 18:37:06 +0200

    添加还是我的回合的叠层效果 一级30 Tick回收 满级4层0Tick回收

--- BuildInBuff/Positive/StillMyTurn.cs
+++ BuildInBuff/Positive/StillMyTurn.cs
@@ -8,12 +8,18 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 using RandomBuff;
 using BuiltinBuffs.Positive;
+using System.ComponentModel;
+using RWCustom;
 
 namespace TemplateGains
 {
     //我的回合
     class StillMyTurnBuff : Buff<StillMyTurnBuff, StillMyTurnBuffData> { public override BuffID ID => StillMyTurnBuffEntry.StillMyTurnID; }
-    class StillMyTurnBuffData : BuffData { public override BuffID ID => StillMyTurnBuffEntry.StillMyTurnID; }
+    class StillMyTurnBuffData : BuffData
+    {
+        public override bool CanStackMore()=> StackLayer < 4;
+        public override BuffID ID => StillMyTurnBuffEntry.StillMyTurnID;
+    }
     class StillMyTurnBuffEntry : IBuffEntry
     {
         public static BuffID StillMyTurnID = new BuffID("StillMyTurnID", true);
@@ -32,23 +38,36 @@ namespace TemplateGains
         private static void Weapon_Update(On.Weapon.orig_Update orig, Weapon self, bool eu)
         {
             orig.Invoke(self, eu);
-            if (self.myTurnWeapon().canWarp && (self.mode == Weapon.Mode.StuckInCreature || BounceSpearBuff.Instance != null))
+            if (self.myTurnWeapon().canWarp)
             {
-                WeaponWarp(self,self.myTurnWeapon().player);
+                WeaponWarp(self, self.myTurnWeapon().player);
             }
         }
 
-        public static void WeaponWarp(Weapon weapon,Player slug)
+        public static void WeaponWarp(Weapon weapon, Player slug)
         {
-            weapon.myTurnWeapon().canWarp = false;
+            var myTurnData = weapon.myTurnWeapon();
+            myTurnData.canWarp = false;
 
+            if (weapon.grabbedBy.Count > 0)
+                return;
             if (slug == null || slug.room == null || !slug.Consious)
                 return;
 
             if (weapon is Spear spear && spear.stuckInObject != null && spear.stuckInObject is Creature creature && creature.room == null)
                 return;
 
-            weapon.firstChunk.pos = slug.firstChunk.pos;
+
+            if (myTurnData.WarpCount > 0)
+            {
+                weapon.ChangeMode(Weapon.Mode.Free);
+                weapon.SetRandomSpin();
+                weapon.firstChunk.pos = Vector2.Lerp(slug.firstChunk.pos, weapon.firstChunk.pos, Mathf.InverseLerp(0, myTurnData.MaxWarpCount, myTurnData.WarpCount));
+                myTurnData.canWarp = true;
+                myTurnData.WarpCount--;
+                return;
+            }
+
             if (slug.FreeHand() != -1)
             {
                 slug.SlugcatGrab(weapon, slug.FreeHand());
@@ -66,13 +85,13 @@ namespace TemplateGains
         private static bool MyTurn_HitSomething(On.Spear.orig_HitSomething orig, Spear self, SharedPhysics.CollisionResult result, bool eu)
         {
             bool hit = orig.Invoke(self, result, eu);
-            if (hit && self.thrownBy is Player player && !(self is ExplosiveSpear) && BounceSpearBuff.Instance == null) 
+            if (hit && self.thrownBy is Player player && !(self is ExplosiveSpear) && BounceSpearBuff.Instance == null)
                 self.myTurnWeapon().CanWarp(player);
 
             return hit;
         }
 
-        
+
     }
     public class EXWeapon
     {
@@ -80,10 +99,13 @@ namespace TemplateGains
         public Player player;
         public bool canWarp = false;
 
+        public int WarpCount = 0;
+        public int MaxWarpCount => 40 - (StillMyTurnBuffEntry.StillMyTurnID.GetBuffData().StackLayer * 10);
         public void CanWarp(Player player)
         {
-            this.player= player;
-            canWarp= true;
+            this.player = player;
+            canWarp = true;
+            WarpCount = MaxWarpCount;
         }
 
 
@@ -96,7 +118,7 @@ namespace TemplateGains
     {
         private static readonly ConditionalWeakTable<Weapon, EXWeapon> modules = new ConditionalWeakTable<Weapon, EXWeapon>();
 
-        public static EXWeapon myTurnWeapon(this Weapon weapon)=> modules.GetValue(weapon, (Weapon s) => new EXWeapon(weapon));
+        public static EXWeapon myTurnWeapon(this Weapon weapon) => modules.GetValue(weapon, (Weapon s) => new EXWeapon(weapon));
 
 
     }
