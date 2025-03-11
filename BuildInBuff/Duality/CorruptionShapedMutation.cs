0a657631094deaa5da664d1f816f3fd35e7d749e
Quaeledy <Quaeledy@163.com>
2025-03-11 23:56:05 +0800

    修复了菌形突变buff、矛矛雨buff、蜘蛛恐惧症buff的一些问题

--- BuildInBuff/Duality/CorruptionShapedMutation.cs
+++ BuildInBuff/Duality/CorruptionShapedMutation.cs
@@ -125,7 +125,7 @@ namespace BuiltinBuffs.Duality
         {
             get
             {
-                return CorruptionShapedMutation.GetBuffData().StackLayer;
+                return CorruptionShapedMutation.GetBuffData()?.StackLayer ?? 0;
             }
         }
 
@@ -832,7 +832,7 @@ namespace BuiltinBuffs.Duality
             for (int i = 0; i < Mathf.Min(player.bodyChunks.Length, this.tentacles.Length); i++)
                 this.tentacles[i] = new CorruptionCatTentacle(player, this, player.bodyChunks[i], this.tentaclesLength, i, Custom.DegToVec(i * 60f + 30f));
             if (this.tentacles.Length > player.bodyChunks.Length)
-                for (int i = player.bodyChunks.Length; i < player.bodyChunks.Length + this.coreChunks.Length; i++)
+                for (int i = player.bodyChunks.Length; i < Mathf.Min(player.bodyChunks.Length + this.coreChunks.Length, this.tentacles.Length); i++)
                     this.tentacles[i] = new CorruptionCatTentacle(player, this, this.coreChunks[i - player.bodyChunks.Length], this.tentaclesLength, i, Custom.DegToVec(i * 60f + 30f));
             if (this.tentacles.Length > player.bodyChunks.Length + this.coreChunks.Length)
                 for (int i = player.bodyChunks.Length + this.coreChunks.Length; i < this.tentacles.Length; i++)
